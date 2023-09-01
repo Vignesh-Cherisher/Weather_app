90df430bf6948de997b15dc8f9b3f72de6b6a2e9
Vignesh-Cherisher <vignesh.muruganandan@solitontech.com>
2023-09-01 15:26:10 +0530

    Uploaded on 01-09-2023

--- files/WeatherApp.js
+++ files/WeatherApp.js
@@ -1,23 +1,39 @@
-const scrollable = document.getElementsByClassName('card-self');
-const scrollOverlay = document.getElementsByClassName('card-overlay');
-const scrollRack = document.querySelector('.cards-rack');
+let cityImage = document.querySelector('#city-image').firstChild
+const scrollable = document.getElementsByClassName('card-self')
+const scrollOverlay = document.getElementsByClassName('card-overlay')
+const scrollRack = document.querySelector('.cards-rack')
 
-function yScroll( x , y) {
-    x.addEventListener("wheel", (evt) => {
-        evt.preventDefault();
-        if(evt.deltaY != 0){
-            window.scrollBy({
-                top: evt.deltaY ,
-            });
-        }
-        if(evt.deltaX != 0){ 
-            y.scrollLeft += evt.deltaX;
-        }
-    })
+/**
+ * @returns {object} - json file data
+ */
+async function fetchJson () {
+  const jsonData = await fetch('./data.json').then((res) => {return res.json()})
 }
 
-for(var i = 0 ; i < scrollable.length ; i++) {
-    yScroll(scrollable[i],scrollRack);    
+fetchJson()
+
+/**
+ * Method to scroll Middle section both Horizontally and Vertically
+ * @param {object} target - target element
+ * @param {object} targetContainer - underlying container
+ */
+function yScroll (target, targetContainer) {
+  target.addEventListener('wheel', (evt) => {
+    console.log(typeof (target), typeof (targetContainer))
+    evt.preventDefault()
+    if (evt.deltaY !== 0) {
+      window.scrollBy({
+        top: evt.deltaY
+      })
+    }
+    if (evt.deltaX !== 0) {
+      targetContainer.scrollLeft += evt.deltaX
+    }
+  })
+}
+
+for (let i = 0; i < scrollable.length; i++) {
+  yScroll(scrollable[i], scrollRack)
 }
 
 yScroll(scrollOverlay[0], scrollRack)
