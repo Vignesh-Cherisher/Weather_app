099e6b35d7bdee3a130f0d9e7a3c9d7e552782ef
Vignesh-Cherisher <vignesh.muruganandan@solitontech.com>
2023-09-06 00:01:23 +0530

    Uploaded on 06-09-2023 home

--- files/WeatherApp.js
+++ files/WeatherApp.js
@@ -7,6 +7,8 @@ let cityInput = document.getElementsByClassName('drop-down')[0]
 let cityDate = document.querySelector('#date')
 let cityTime = document.querySelector('.time').children[0]
 let citySeconds = document.querySelector('#seconds')
+let cityAmPm = document.querySelector('#am-pm')
+let toggleAmPm = 0
 ;
 
 //Asynchronous Function to load json Data
@@ -17,6 +19,8 @@ let citySeconds = document.querySelector('#seconds')
   const response = await fetch('./data.json')
   const jsonData = await response.json()
   datalistPopulate(jsonData)
+  changeCityImg(jsonData.nil)
+  changeCityDateTime(jsonData.nil)
   citySelect(jsonData)
 })()
 
@@ -34,7 +38,7 @@ function citySelect(jsonData){
   cityInput.addEventListener("input", function(event){
     let val = cityInput.value.toLowerCase()
     changeCityImg(jsonData[val])
-    updateCityDateTime(jsonData[val])
+    changeCityDateTime(jsonData[val])
   })
 }
 
@@ -49,37 +53,129 @@ function changeCityImg(jsonCityName) {
 }
 
 //Method to get and parse time and Date of selected cities
-function updateCityDateTime(jsonCityName) {
+function changeCityDateTime(jsonCityName) {
   let jsonDateTime = jsonCityName.dateAndTime
   jsonDateTime = jsonDateTime.split(' ')
   let jsonTime = jsonDateTime[1]
   let jsonDate = jsonDateTime[0].slice(0,-1)
-  updateCityTime(jsonTime)
-  updateCityDate(jsonDate)
+  if(jsonDateTime[2] == "AM")
+    toggleAmPm = 0
+  else
+    toggleAmPm = 1
+  if(jsonCityName.cityName == "NIL"){
+    changeAmState(NaN)
+    console.log("success")
+  }
+  else
+    changeAmState(toggleAmPm)
+  changeCityTime(jsonTime, jsonCityName.cityName)
+  changeCityDate(jsonDate)
 }
 
-//Method to update City Time
-function updateCityTime(jsonTime){
+//Method to call functions to change City Time
+function changeCityTime(jsonTime, cityName){
   if(isNaN(parseInt(jsonTime))){
     cityTime.innerHTML = jsonTime
     citySeconds.innerHTML = ''
   }
   else {
-    cityTime.innerHTML = jsonTime.slice(0, -3)
-    citySeconds.innerHTML = jsonTime.slice(-3)
+    updateCityTime(jsonTime)
+    runCityTime(jsonTime, cityName)
   }
-  runCityTime(jsonTime)
+}
+
+//Method to update city time
+function updateCityTime(liveTime){
+  cityTime.innerHTML = liveTime.slice(0, -3)
+  citySeconds.innerHTML = liveTime.slice(-3)
 }
 
 //Method to run Live Time of selected City
-function runCityTime(jsonTime){
-  jsonTime = jsonTime.split(':')
-  let liveTime = setTimeout(function(){ currentTime() }, 1000);
-  updatedCityTime(liveTime)
+function runCityTime(liveTime, cityName){
+  liveTime = liveTime.split(':')
+  liveTime.forEach((element,index) => {
+    liveTime[index] = parseInt(element)
+  });
+  const interval = setInterval(function(){ 
+    if( (cityName != cityInput.value) && (cityName != cityInput.placeholder)) {
+      cityName = cityInput.value
+      clearInterval(interval);
+    }
+    else
+      updateCityTime(incrementSecond(liveTime));
+  }, 1000);
+}
+
+//Method to increment Second
+function incrementSecond(liveTime) {
+  if(liveTime[2] >= 59) {
+    incrementMinute(liveTime)
+    liveTime[2] = 0
+  }
+  else 
+    liveTime[2]++
+  liveTime.forEach((element,index) => {
+    liveTime[index] = element.toString()
+    if(liveTime[index].length < 2 && index != 0){
+      console.log(liveTime[index]);
+      liveTime[index] = '0' + element
+    }
+  });
+  liveTime = liveTime.join(':')
+  return liveTime
+}
+
+//Method to increment Minute
+function incrementMinute(liveTime) {
+  if(liveTime[1] >= 59) {
+    incrementHour(liveTime)
+    liveTime[1] = 0
+  }
+  else 
+    liveTime[1]++
+  return liveTime
+}
+
+//Method to increment Hour
+function incrementHour(liveTime) {
+  if(liveTime[0] >= 12) {
+    changeAmState(!toggleAmPm)
+    liveTime[0] = 1
+  }
+  else 
+    liveTime[0]++
+  return liveTime
+}
+
+//Method to set AM or PM icon for city
+function changeAmState(toggleAmPm) {
+  if(isNaN(toggleAmPm)) {
+    cityAmPm.src = "../General_Images_&_Icons/ampmState.jpeg"
+    cityAmPm.classList.remove('am-pm')
+    cityAmPm.classList.add('am-pm-nil')
+    cityTime.style.color = "#fff"
+    citySeconds.style.color = "#fff"
+  }
+  else if(!toggleAmPm){
+    cityAmPm.src =  "../General_Images_&_Icons/amState.svg"
+    cityTime.style.color = "#ffe5b4";
+    citySeconds.style.color = "#ffe5b4";
+    cityAmPm.classList.add('am-pm')
+    cityAmPm.classList.remove('am-pm-nil')
+    toggleAmPm = !toggleAmPm
+  }
+  else {
+    cityAmPm.src = "../General_Images_&_Icons/pmState.svg"
+    cityTime.style.color = "#1E90FF"
+    citySeconds.style.color = "#1E90FF"
+    cityAmPm.classList.add('am-pm')
+    cityAmPm.classList.remove('am-pm-nil')
+    toggleAmPm = !toggleAmPm
+  }
 }
 
 //Method to update City date
-function updateCityDate(jsonDate) {
+function changeCityDate(jsonDate) {
 
 }
 
