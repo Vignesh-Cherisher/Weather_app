b928e59db4826e0c3f168fb876807920f1763740
Vignesh-Cherisher <vignesh.muruganandan@solitontech.com>
2023-09-07 17:57:46 +0530

    Uploaded on 07-09-2023

--- files/WeatherApp.js
+++ files/WeatherApp.js
@@ -1,28 +1,29 @@
-let cityImage = document.querySelector('#city-image').children[0]
+const cityImage = document.querySelector('#city-image').children[0]
 const scrollable = document.getElementsByClassName('card-self')
 const scrollOverlay = document.getElementsByClassName('card-overlay')
 const scrollRack = document.querySelector('.cards-rack')
-let cityValue = document.querySelector('#cities')
-let cityInput = document.getElementsByClassName('drop-down')[0]
-let cityDate = document.querySelector('#date')
-let cityTime = document.querySelector('.time').children[0]
-let citySeconds = document.querySelector('#seconds')
-let cityAmPm = document.querySelector('#am-pm')
-let forecastedValues = document.querySelectorAll('.forecast-values')
-let forecastedTemperature = document.querySelectorAll('.forecast-temperature')
-let scaleTime = document.querySelectorAll('.scale-time')
+const cityValue = document.querySelector('#cities')
+const cityInput = document.getElementsByClassName('drop-down')[0]
+const cityDate = document.querySelector('#date')
+const cityTime = document.querySelector('.time').children[0]
+const citySeconds = document.querySelector('#seconds')
+const cityAmPm = document.querySelector('#am-pm')
+const forecastedValues = document.querySelectorAll('.forecast-values')
+const forecastedTemperature = document.querySelectorAll('.forecast-temperature')
+const scaleTime = document.querySelectorAll('.scale-time')
+const scaleWeatherIcon = document.querySelectorAll('.weather')
 let toggleAmPm = 0
 ;
 
-//Asynchronous Function to load json Data
+// Asynchronous Function to load json Data
 /**
  *
  */
-( async () => {
+(async () => {
   const response = await fetch('./data.json')
   const jsonData = await response.json()
   datalistPopulate(jsonData)
-  keepDatalistOptions('.drop-down',jsonData)
+  keepDatalistOptions('.drop-down', jsonData)
   changeCityImg(jsonData.nil)
   changeCityDateTime(jsonData.nil)
   changeForecastValues(jsonData.nil)
@@ -30,21 +31,29 @@ let toggleAmPm = 0
   citySelect(jsonData)
 })()
 
-//Method to add option values from json to Datalist
-function datalistPopulate(jsonData) { 
-  for(let city in jsonData) {
-    let option = document.createElement('option')
+// Method to add option values from json to Datalist
+/**
+ *
+ * @param {object} jsonData - Data loaded from json
+ */
+function datalistPopulate (jsonData) {
+  for (const city in jsonData) {
+    const option = document.createElement('option')
     option.value = jsonData[city].cityName
     cityValue.appendChild(option)
   }
 }
 
-//Method to call Functions for updating values whenever City name is changed
-function citySelect(jsonData){
-  cityInput.addEventListener("input", function(event){
-    for(let city in jsonData){
-      if(event.target.value == jsonData[city].cityName){
-        let val = cityInput.value.toLowerCase()
+// Method to call Functions for updating values whenever City name is changed
+/**
+ *
+ * @param {object} jsonData - Data loaded from json
+ */
+function citySelect (jsonData) {
+  cityInput.addEventListener('input', function (event) {
+    for (const city in jsonData) {
+      if (event.target.value === jsonData[city].cityName) {
+        const val = cityInput.value.toLowerCase()
         changeCityImg(jsonData[val])
         changeCityDateTime(jsonData[val])
         changeForecastValues(jsonData[val])
@@ -54,129 +63,148 @@ function citySelect(jsonData){
   })
 }
 
-//Changing City Image dynamically in top section
-function changeCityImg(jsonCityEntry) {
-  if(jsonCityEntry == "nil")
-    cityImage.src = '../Icons_for_cities/placeholder.png'
-  else {
-    let cityImgSource = jsonCityEntry.url
+// Changing City Image dynamically in top section
+/**
+ *
+ * @param {object} jsonCityEntry - Specific City's key value pairs
+ */
+function changeCityImg (jsonCityEntry) {
+  if (jsonCityEntry === 'nil') { cityImage.src = '../Icons_for_cities/placeholder.png' } else {
+    const cityImgSource = jsonCityEntry.url
     cityImage.src = '../Icons_for_cities/' + cityImgSource
   }
 }
 
-//Method to get and parse time and Date of selected cities
-function changeCityDateTime(jsonCityEntry) {
+// Method to get and parse time and Date of selected cities
+/**
+ *
+ * @param {object} jsonCityEntry - Specific City's key value pairs
+ */
+function changeCityDateTime (jsonCityEntry) {
   let jsonDateTime = jsonCityEntry.dateAndTime
   jsonDateTime = jsonDateTime.split(' ')
-  let jsonTime = jsonDateTime[1]
-  let jsonDate = jsonDateTime[0].slice(0,-1)
-  if(jsonDateTime[2] == "AM")
-    toggleAmPm = 0
-  else
-    toggleAmPm = 1
-  if(jsonCityEntry.cityName == "NIL")
-    changeAmState(NaN)
-  else
-    changeAmState(toggleAmPm)
+  const jsonTime = jsonDateTime[1]
+  const jsonDate = jsonDateTime[0].slice(0, -1)
+  if (jsonDateTime[2] === 'AM') { toggleAmPm = 0 } else { toggleAmPm = 1 }
+  if (jsonCityEntry.cityName === 'NIL') { changeAmState(NaN) } else { changeAmState(toggleAmPm) }
   changeCityTime(jsonTime, jsonCityEntry.cityName)
-  changeCityDate(jsonDate,jsonCityEntry.cityName)
+  changeCityDate(jsonDate, jsonCityEntry.cityName)
 }
 
-//Method to call functions to change City Time
-function changeCityTime(jsonTime, cityName){
-  if(isNaN(parseInt(jsonTime))){
+// Method to call functions to change City Time
+/**
+ *
+ * @param {string} jsonTime - to pass selected city time
+ * @param {string} cityName - to pass selected city name
+ */
+function changeCityTime (jsonTime, cityName) {
+  if (isNaN(parseInt(jsonTime))) {
     cityTime.innerHTML = jsonTime
     citySeconds.innerHTML = ''
-  }
-  else {
+  } else {
     updateCityTime(jsonTime)
     runCityTime(jsonTime, cityName)
   }
-  }
+}
 
-//Method to update city time
-function updateCityTime(liveTime){
+// Method to update city time
+/**
+ *
+ * @param {object} liveTime - to pass Time object for live time
+ */
+function updateCityTime (liveTime) {
   cityTime.innerHTML = liveTime.slice(0, -3)
   citySeconds.innerHTML = liveTime.slice(-3)
 }
 
-//Method to run Live Time of selected City
-function runCityTime(liveTime, cityName){
+// Method to run Live Time of selected City
+/**
+ *
+ * @param {object} liveTime - to pass Time object for live time
+ * @param {string} cityName - to pass selected city name
+ */
+function runCityTime (liveTime, cityName) {
   liveTime = liveTime.split(':')
-  liveTime.forEach((element,index) => {
+  liveTime.forEach((element, index) => {
     liveTime[index] = parseInt(element)
-  });
-  const interval = setInterval(function(){ 
-    if( (cityName != cityInput.value) && (cityName != cityInput.placeholder)) {
+  })
+  const interval = setInterval(function () {
+    if ((cityName !== cityInput.value) && (cityName !== cityInput.placeholder)) {
       cityName = cityInput.value
-      clearInterval(interval);
-    }
-    else
-      updateCityTime(incrementSecond(liveTime));
-  }, 1000);
+      clearInterval(interval)
+    } else { updateCityTime(incrementSecond(liveTime)) }
+  }, 1000)
 }
 
-//Method to increment Second
-function incrementSecond(liveTime) {
-  if(liveTime[2] >= 59) {
+// Method to increment Second
+/**
+ * @returns {object} - return live time after updation
+ * @param {object} liveTime - to pass Time object for live time
+ */
+function incrementSecond (liveTime) {
+  if (liveTime[2] >= 59) {
     incrementMinute(liveTime)
     liveTime[2] = 0
-  }
-  else 
-    liveTime[2]++
-  //Convert each element to String and add leading zeroes if necessary
-  liveTime.forEach((element,index) => {
+  } else { liveTime[2]++ }
+  // Convert each element to String and add leading zeroes if necessary
+  liveTime.forEach((element, index) => {
     liveTime[index] = element.toString()
-    if(liveTime[index].length < 2 && index != 0)
-      liveTime[index] = '0' + element
-  });
+    if (liveTime[index].length < 2 && index !== 0) { liveTime[index] = '0' + element }
+  })
   liveTime = liveTime.join(':')
   return liveTime
 }
 
-//Method to increment Minute
-function incrementMinute(liveTime) {
-  if(liveTime[1] >= 59) {
+// Method to increment Minute
+/**
+ * @returns {object} - return live time after updation
+ * @param {object} liveTime - to pass Time object for live time
+ */
+function incrementMinute (liveTime) {
+  if (liveTime[1] >= 59) {
     incrementHour(liveTime)
     liveTime[1] = 0
-  }
-  else 
-    liveTime[1]++
+  } else { liveTime[1]++ }
   return liveTime
 }
 
-//Method to increment Hour
-function incrementHour(liveTime) {
-  if(liveTime[0] >= 12) {
+// Method to increment Hour
+/**
+ * @returns {object} - return live time after updation
+ * @param {object} liveTime - to pass Time object for live time
+ */
+function incrementHour (liveTime) {
+  if (liveTime[0] >= 12) {
     changeAmState(!toggleAmPm)
     liveTime[0] = 1
-  }
-  else 
-    liveTime[0]++
+  } else { liveTime[0]++ }
   return liveTime
 }
 
-//Method to set AM or PM icon for city
-function changeAmState(toggleAmPm) {
-  if(isNaN(toggleAmPm)) {
-    cityAmPm.src = "../General_Images_&_Icons/ampmState.png"
+// Method to set AM or PM icon for city
+/**
+ *
+ * @param {number} toggleAmPm - to identify 'am' or 'pm'
+ */
+function changeAmState (toggleAmPm) {
+  if (isNaN(toggleAmPm)) {
+    cityAmPm.src = '../General_Images_&_Icons/ampmState.png'
     cityAmPm.classList.remove('am-pm')
     cityAmPm.classList.add('am-pm-nil')
     cityTime.classList.add('time-gradient')
-  }
-  else if(!toggleAmPm){
-    cityAmPm.src =  "../General_Images_&_Icons/amState.svg"
-    cityTime.style.color = "#ffe5b4";
-    citySeconds.style.color = "#ffe5b4";
+    cityTime.setAttribute('style', '-webkit-background-clip:text')
+  } else if (!toggleAmPm) {
+    cityAmPm.src = '../General_Images_&_Icons/amState.svg'
+    cityTime.style.color = '#ffe5b4'
+    citySeconds.style.color = '#ffe5b4'
     cityAmPm.classList.add('am-pm')
     cityAmPm.classList.remove('am-pm-nil')
     cityTime.classList.remove('time-gradient')
     toggleAmPm = !toggleAmPm
-  }
-  else {
-    cityAmPm.src = "../General_Images_&_Icons/pmState.svg"
-    cityTime.style.color = "#1E90FF"
-    citySeconds.style.color = "#1E90FF"
+  } else {
+    cityAmPm.src = '../General_Images_&_Icons/pmState.svg'
+    cityTime.style.color = '#1E90FF'
+    citySeconds.style.color = '#1E90FF'
     cityAmPm.classList.add('am-pm')
     cityAmPm.classList.remove('am-pm-nil')
     cityTime.classList.remove('time-gradient')
@@ -184,71 +212,101 @@ function changeAmState(toggleAmPm) {
   }
 }
 
-//Method to update City date
-function changeCityDate(jsonDate, cityName) {
-  if(cityName == "NIL"){
+// Method to update City date
+/**
+ *
+ * @param {object} jsonDate - to pass selected city date
+ * @param {string} cityName - to pass selected city name
+ */
+function changeCityDate (jsonDate, cityName) {
+  if (cityName === 'NIL') {
     cityDate.innerHTML = jsonDate
-  }
-  else {
-    let dateParts = jsonDate.split('/')
-    let cityDateVar = new Date(+dateParts[2], dateParts[0]-1, +dateParts[1]);
+  } else {
+    const dateParts = jsonDate.split('/')
+    let cityDateVar = new Date(+dateParts[2], dateParts[0] - 1, +dateParts[1])
     cityDateVar = cityDateVar.toString().split(' ')
     cityDate.innerHTML = cityDateVar[2] + '-' + cityDateVar[1] + '-' + cityDateVar[3]
   }
 }
 
-//Method to update forecasted values
-function changeForecastValues(jsonCityEntry) {
+// Method to update forecasted values
+/**
+ *
+ * @param {object} jsonCityEntry - Specific City's key value pairs
+ */
+function changeForecastValues (jsonCityEntry) {
   forecastedValues[0].innerHTML = jsonCityEntry.temperature
-  if(jsonCityEntry.cityName == "NIL")
-    forecastedValues[1].innerHTML = jsonCityEntry.temperature.slice(0,-2) + ' F'
-  else {
-    let Fahrenheit = ((parseInt(jsonCityEntry.temperature) * 1.8) + 32).toFixed(0) + ' F'
-    forecastedValues[1].innerHTML = Fahrenheit;
+  if (jsonCityEntry.cityName === 'NIL') { forecastedValues[1].innerHTML = jsonCityEntry.temperature.slice(0, -2) + ' F' } else {
+    const fahrenheit = ((parseInt(jsonCityEntry.temperature) * 1.8) + 32).toFixed(0) + ' F'
+    forecastedValues[1].innerHTML = fahrenheit
   }
-  forecastedValues[2].children[0].innerHTML = jsonCityEntry.humidity.slice(0,-1)
-  forecastedValues[3].children[0].innerHTML = jsonCityEntry.precipitation.slice(0,-1)
+  forecastedValues[2].children[0].innerHTML = jsonCityEntry.humidity.slice(0, -1)
+  forecastedValues[3].children[0].innerHTML = jsonCityEntry.precipitation.slice(0, -1)
 }
 
-//Method to update timeline for forecasting next 5 hours
-function changeForecastTimeline(jsonCityEntry) {
-  if(jsonCityEntry.cityName != "NIL"){
+// Method to update timeline for forecasting next 5 hours
+/**
+ *
+ * @param {object} jsonCityEntry - Specific City's key value pairs
+ */
+function changeForecastTimeline (jsonCityEntry) {
+  if (jsonCityEntry.cityName !== 'NIL') {
     changeTimelineHours()
     changeTimelineTemperature(jsonCityEntry)
-  }
-  else{
-    for(let i = 0; i < scaleTime.length; i++){
-      scaleTime[i].innerHTML = "♾️"
+    changeTimelineIcon(jsonCityEntry)
+  } else {
+    for (let i = 0; i < scaleTime.length; i++) {
+      scaleTime[i].innerHTML = '♾️'
     }
-    for(let i = 0; i < forecastedTemperature.length; i++) {
+    for (let i = 0; i < forecastedTemperature.length; i++) {
       forecastedTemperature[i].innerHTML = jsonCityEntry.nextFiveHrs[1]
     }
   }
 }
 
-//Method to change next 5 hours in Forecast Timeline
-function changeTimelineHours() {
-  let cityHour = parseInt(cityTime.innerHTML.slice(0,-3)) + 1
-  let forecastAmPm = []
+// Method to change next 5 hours in Forecast Timeline
+/**
+ *
+ */
+function changeTimelineHours () {
+  let cityHour = parseInt(cityTime.innerHTML.slice(0, -3)) + 1
+  const forecastAmPm = []
   forecastAmPm[0] = toggleAmPm
-  forecastAmPm[1] = (toggleAmPm ? " PM" : " AM");
-  for(let i = 0; i < scaleTime.length; i++){
-    if(cityHour > 12)
-      cityHour = 1;
-    if(cityHour == 12){
+  forecastAmPm[1] = (toggleAmPm ? ' PM' : ' AM')
+  for (let i = 0; i < scaleTime.length; i++) {
+    if (cityHour > 12) { cityHour = 1 }
+    if (cityHour === 12) {
       forecastAmPm[0] = !forecastAmPm[0]
-      forecastAmPm[1] = (forecastAmPm[0] ? " PM" : " AM");
+      forecastAmPm[1] = (forecastAmPm[0] ? ' PM' : ' AM')
     }
     scaleTime[i].innerHTML = cityHour + forecastAmPm[1]
-    cityHour ++
+    cityHour++
   }
 }
 
-//Method to change forecasted Temperature in Forecast Timeline
-function changeTimelineTemperature(jsonCityEntry) {
+// Method to change forecasted Temperature in Forecast Timeline
+/**
+ *
+ * @param {object} jsonCityEntry - Specific City's key value pairs
+ */
+function changeTimelineTemperature (jsonCityEntry) {
   forecastedTemperature[0].innerHTML = jsonCityEntry.temperature
-  for(let i = 1; i < forecastedTemperature.length - 1; i++) {
-    forecastedTemperature[i].innerHTML = jsonCityEntry.nextFiveHrs[i-1]
+  for (let i = 1; i < forecastedTemperature.length; i++) {
+    forecastedTemperature[i].innerHTML = jsonCityEntry.nextFiveHrs[i - 1]
+  }
+}
+
+// Method to change weather icons in Forecast Timeline
+/**
+ *
+ * @param {object} jsonCityEntry - Specific City's key value pairs
+ */
+function changeTimelineIcon (jsonCityEntry) {
+  const cityTemperature = []
+  cityTemperature[0] = parseInt(jsonCityEntry.temperature)
+  for (let i = 0; i < jsonCityEntry.nextFiveHrs.length; i++) { cityTemperature[i + 1] = parseInt(jsonCityEntry.nextFiveHrs[i]) }
+  for (let i = 0; i < cityTemperature.length; i++) {
+    if (cityTemperature[i] > 29) { scaleWeatherIcon[i].src = '../Weather_Icons/sunnyIcon.svg' } else if ((cityTemperature[i] <= 29) && (cityTemperature[i] >= 23)) { scaleWeatherIcon[i].src = '../Weather_Icons/cloudyIcon.svg' } else if ((cityTemperature[i] <= 22) && (cityTemperature[i] >= 18)) { scaleWeatherIcon[i].src = '../Weather_Icons/windyIcon.svg' } else { scaleWeatherIcon[i].src = '../Weather_Icons/precipitationIcon.svg' }
   }
 }
 
@@ -271,28 +329,33 @@ function yScroll (target, targetContainer) {
   })
 }
 
-//Method to blur and handle click event in Input tag for selecting city.
-function keepDatalistOptions(selector = '',jsonData) {
+// Method to blur and handle click event in Input tag for selecting city.
+/**
+ *
+ * @param {string} selector - String to pass class name of Input tag
+ * @param {object} jsonData - to pass Entire json Data
+ */
+function keepDatalistOptions (selector = '', jsonData) {
   // select all input fields by datalist attribute or by class/id
-  let datalistInputs = document.querySelectorAll(selector);
+  const datalistInputs = document.querySelectorAll(selector)
   if (datalistInputs.length) {
     for (let i = 0; i < datalistInputs.length; i++) {
-      let input = datalistInputs[i];
-      input.addEventListener("input", function(e) {
-        for(let city in jsonData){
-          if(e.target.value == jsonData[city].cityName){
-            e.target.setAttribute("placeholder", e.target.value);
-            e.target.blur();
+      const input = datalistInputs[i]
+      input.addEventListener('input', function (e) {
+        for (const city in jsonData) {
+          if (e.target.value === jsonData[city].cityName) {
+            e.target.setAttribute('placeholder', e.target.value)
+            e.target.blur()
           }
         }
-      });
-      input.addEventListener("focus", function(e) {
-        e.target.setAttribute("placeholder", e.target.value);
-        e.target.value = "";
-      });
-      input.addEventListener("blur", function(e) {
-        e.target.value = e.target.getAttribute("placeholder");
-      });
+      })
+      input.addEventListener('focus', function (e) {
+        e.target.setAttribute('placeholder', e.target.value)
+        e.target.value = ''
+      })
+      input.addEventListener('blur', function (e) {
+        e.target.value = e.target.getAttribute('placeholder')
+      })
     }
   }
 }
